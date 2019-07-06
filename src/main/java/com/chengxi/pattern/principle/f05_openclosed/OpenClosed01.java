601d795eaf70c31fbc5497a6a9198d5b4a6d022b
jiangjc <jiangjc3@foxmail.com>
2019-07-06 22:41:37 +0800

    update

---
+++ src/main/java/com/chengxi/pattern/principle/f05_openclosed/OpenClosed01.java
@@ -0,0 +1,50 @@
+package com.chengxi.pattern.principle.f05_openclosed;
+
+/**
+ * Created By CHENGXI on 2019/7/6
+ */
+public class OpenClosed01 {
+    public static void main(String[] args) {
+        GrapicEditor grapicEditor = new GrapicEditor();
+        grapicEditor.drawShape(new Rectangle());
+        grapicEditor.drawShape(new Circle());
+    }
+}
+// 这是一个绘图的类
+class GrapicEditor {
+    public void drawShape(Shape s) {
+        if (s.type == 1) {
+            drawRectangle();
+        } else if (s.type == 2) {
+            drawCycle();
+        } // 新增图形类型就得新增分支，并且新增方法
+    }
+    private void drawRectangle() {
+        System.out.println("绘制矩形");
+    }
+    private void drawCycle() {
+        System.out.println("绘制圆形");
+    }
+}
+// Shape类，基类
+class Shape {
+    int type;
+}
+// 矩形
+class Rectangle extends Shape {
+    Rectangle() {
+        super.type = 1;
+    }
+}
+// 圆形
+class Circle extends Shape {
+    Circle() {
+        super.type = 2;
+    }
+}
+// 新增一个绘制三形的功能
+class Triangle extends Shape {
+    Triangle () {
+        super.type = 3;
+    }
+}
---
+++ src/main/java/com/chengxi/pattern/principle/f05_openclosed/OpenClosed02.java
@@ -0,0 +1,58 @@
+package com.chengxi.pattern.principle.f05_openclosed;
+
+/**
+ * Created By CHENGXI on 2019/7/6
+ */
+public class OpenClosed02 {
+    public static void main(String[] args) {
+        GrapicEditor02 grapicEditor = new GrapicEditor02();
+        grapicEditor.drawShape(new Rectangle02());
+        grapicEditor.drawShape(new Circle02());
+        grapicEditor.drawShape(new Triangle02());
+    }
+}
+class GrapicEditor02 {
+    public void drawShape(Shape02 s) {
+        s.draw();
+    }
+
+}
+// Shape类，基类
+abstract class Shape02 {
+    int type;
+    public abstract void draw();
+}
+
+// 矩形
+class Rectangle02 extends Shape02 {
+    Rectangle02() {
+        super.type = 1;
+    }
+    @Override
+    public void draw() {
+        System.out.println("绘制矩形");
+    }
+}
+
+// 圆形
+class Circle02 extends Shape02 {
+    Circle02() {
+        super.type = 2;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("绘制圆形");
+    }
+}
+
+// 新增一个绘制三形的功能
+class Triangle02 extends Shape02 {
+    Triangle02 () {
+        super.type = 3;
+    }
+    @Override
+    public void draw() {
+        System.out.println("绘制三角形");
+    }
+}
