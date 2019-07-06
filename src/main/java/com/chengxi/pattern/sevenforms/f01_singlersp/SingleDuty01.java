bec0e4274418b2091e88de5b3a49443430ee7193
jiangjc <jiangjc3@foxmail.com>
2019-07-06 14:48:12 +0800

    update

---
+++ src/main/java/com/chengxi/pattern/sevenforms/f01_singlersp/SingleDuty01.java
@@ -0,0 +1,31 @@
+package com.chengxi.pattern.sevenforms.f01_singlersp;
+
+/**
+ * Created By CHENGXI on 2019/7/6
+ */
+public class SingleDuty01 {
+    /**
+     * 设计模式七大原则
+     * 1 单一职责原则
+     *   一般是对于类来说， 一个类应该只负责一个职责。或者可以定义为：就一个类而言，应该只有一个引起它变化的原因。
+     *   如果类A负责两个不同的职责1 2， 如果职责1需求更改时， 有可能造成职责2执行出错
+     *   所以应该将类A的粒度分解为A1 A2
+     */
+
+    public static void main(String[] args) {
+        SuperStar01 superstar = new SuperStar01();
+        superstar.play("姚明");
+        superstar.play("宋小宝");
+    }
+}
+
+/**
+ * 方式1
+ * 1. 在方式1的play方法中，违反了单一职责原则
+ * 2. 解决方案： 根据明星的行业不同， 分解成不同的
+ */
+class SuperStar01{
+    public void play(String name) {
+        System.out.println(name + "在NBA打篮球。。。");
+    }
+}
---
+++ src/main/java/com/chengxi/pattern/sevenforms/f01_singlersp/SingleDuty03.java
@@ -0,0 +1,29 @@
+package com.chengxi.pattern.sevenforms.f01_singlersp;
+
+/**
+ * Created By CHENGXI on 2019/7/6
+ */
+public class SingleDuty03 {
+    public static void main(String[] args) {
+        SuperStar03 superstar = new SuperStar03();
+        superstar.play("姚明");
+        superstar.perform("宋小宝");
+    }
+}
+
+/**
+ * 方式3
+ * 1. 这种方式没有对原来的类做大的修改，只是增加方法
+ * 2. 虽然此方式没有在类这个级别上遵守单一职责原则，但是在方法级别上，仍然遵守单一职责原则
+ * 3. 当方法功能很简单时，可以用这种方式
+ */
+class SuperStar03{
+    public void play(String name) {
+        System.out.println(name + "在NBA打篮球。。。");
+
+    }
+
+    public void perform(String name) {
+        System.out.println(name + "在舞台上演小品。。。");
+    }
+}
